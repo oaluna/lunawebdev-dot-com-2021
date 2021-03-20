db0dbc4b3ac709c8e46d6941d986a3a6d7de7871
Oscar Luna <CaporalesLuna@gmail.com>
2021-03-20 13:24:08 -0700

    rebase to 2 commits back

---
+++ src/worksData.js
@@ -0,0 +1,75 @@
+const worksData = [
+  {
+    id: 1,
+    category: 'project',
+    image1: {
+      url:
+        'https://images.pexels.com/photos/1606386/pexels-photo-1606386.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      class: 'pastelImageLarge'
+    },
+    logo: {
+      url: 'https://images.pexels.com/photos/1606386/pexels-photo-1606386.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      class: 'pastelImageSmall'
+    },
+    card: {
+      title: 'Portfolio website created. Visit https://oscarluna.dev',
+      text: 'Total reach:',
+      number: 12000000
+    },
+    info1: {
+      title: 'Portfolio website created. Visit https://oscarluna.dev',
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales purus arcu, sit amet pulvinar mauris vulputate et. Maecenas sed venenatis purus. Phasellus nec semper libero, eget pharetra nulla. Sed viverra maximus lacinia. Sed condimentum lorem orci, vitae finibus nunc viverra a. Suspendisse volutpat imperdiet leo, sit amet tincidunt sapien interdum et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque ultricies eu dui ac finibus.'
+    },
+    info2: {
+      title: null,
+      text: null
+    },
+    info3: {
+      title: null,
+      text: null
+    },
+    tags: {
+      tag1: 'Web',
+      tag2: 'Portfolio',
+      tag3: 'Web Development',
+      tag4: 'For Hire',
+      tag5: 'Front-End Developer'
+    },
+    stats1: {
+      title: 'Campaign Results:',
+
+      url1: null,
+      social1: null,
+      number1: null,
+      tag1: 'and counting',
+
+      url2: null,
+      social2: null,
+      number2: null,
+      tag2: 'and counting',
+
+      url3: null,
+      social3: null,
+      number3: 96,
+      tag3: null,
+
+      url4: null,
+      social4: null,
+      number4: 19,
+      tag4: null,
+
+      url5: null,
+      social5: null,
+      number5: null,
+      tag5: null,
+
+      url6: null,
+      social6: null,
+      number6: null,
+      tag6: null
+    }
+  }
+];
+
+export default worksData;
