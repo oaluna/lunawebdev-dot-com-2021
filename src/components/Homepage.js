27ebd70d995e5e89c3dabac2dbe02c65f00394c9
Oscar Luna <CaporalesLuna@gmail.com>
2021-07-12 08:55:32 -0700

    Uniform Typeface for whole page

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -26,14 +26,16 @@ export default class Homepage extends Component {
             <h1 style={{ display: 'flex', flexDirection: 'column' }}>
               {' '}
               <br />
-              Hello! I'm Oscar,<br />
+              Hello! I'm Oscar,
+              <br />
               and I'm a front-end developer from San Francisco
             </h1>
             <br />
             <br />
             <h3 className='siteSubtitle'>
               {' '}
-              I create, deploy, and maintain web applications. I am also a writer for Medium. I am currently in search of work opportunities.
+              I create, deploy, and maintain web applications. I am also a
+              writer for Medium. I am currently in search of work opportunities.
             </h3>
           </div>
           <div
@@ -145,31 +147,83 @@ export default class Homepage extends Component {
             </div>
           </div>
         </div>
-        <div className="brandsContainer" style={{position: 'relative', marginTop: '125vh'}}>
-        <div className='brandsOverlay' style={{ zIndex: -1 }}>
-          </div>
-        <div className="brandsContent">
-          <div className="brandsTitle">
-            <h1>Technologies I work with:</h1>
-          </div>
+        <div
+          className='brandsContainer'
+          style={{ position: 'relative', marginTop: '125vh' }}>
+          <div className='brandsOverlay' style={{ zIndex: -1 }}></div>
+          <div className='brandsContent'>
+            <div className='brandsTitle'>
+              <h1>Technologies I work with:</h1>
+            </div>
 
-          <div className="brandsList">
-            <img aria-labelledby="ReactJS" src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="ReactJS"/>
-            <img aria-labelledby="NextJS" src="https://cdn.worldvectorlogo.com/logos/nextjs-3.svg" alt="NextJS"/>
-            <img aria-labelledby="Apollo" src="https://cdn.worldvectorlogo.com/logos/apollo-graphql-1.svg" alt="Apollo"/>
-            <img aria-labelledby="GraphQL" src="https://cdn.worldvectorlogo.com/logos/graphql.svg" alt="GraphQL"/>
-            <img aria-labelledby="NodeJS" src="https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg" alt="NodeJS"/>
-            <img aria-labelledby="Redux" src="https://cdn.worldvectorlogo.com/logos/redux.svg" alt="Redux"/>
-            <img aria-labelledby="Sass" src="https://cdn.worldvectorlogo.com/logos/sass-1.svg" alt="Sass"/>
-            <img aria-labelledby="MongoDB" src="https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg"/>
-            <img aria-labelledby="Amazon Web Services" src="https://cdn.worldvectorlogo.com/logos/aws-2.svg" alt="Amazon Web Services" />
-            <img aria-labelledby="JSONWebToken" src="https://cdn.worldvectorlogo.com/logos/jwtio-json-web-token.svg" alt="JSONWebToken" />
-            <img aria-labelledby="NPM" src="https://cdn.worldvectorlogo.com/logos/npm.svg" alt="NPM" />
-            <img aria-labelledby="Figma" src="https://cdn.worldvectorlogo.com/logos/figma-1.svg" alt="Figma" />
-            <img aria-labelledby="Gatsby" src="https://cdn.worldvectorlogo.com/logos/gatsby.svg" alt="Gatsby" />
+            <div className='brandsList'>
+              <img
+                aria-labelledby='ReactJS'
+                src='https://cdn.worldvectorlogo.com/logos/react-2.svg'
+                alt='ReactJS'
+              />
+              <img
+                aria-labelledby='NextJS'
+                src='../assets/nextjs-icon.svg'
+                alt='NextJS'
+              />
+              <img
+                aria-labelledby='Apollo'
+                src='https://cdn.worldvectorlogo.com/logos/apollo-graphql-1.svg'
+                alt='Apollo'
+              />
+              <img
+                aria-labelledby='GraphQL'
+                src='https://cdn.worldvectorlogo.com/logos/graphql.svg'
+                alt='GraphQL'
+              />
+              <img
+                aria-labelledby='NodeJS'
+                src='https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg'
+                alt='NodeJS'
+              />
+              <img
+                aria-labelledby='Redux'
+                src='https://cdn.worldvectorlogo.com/logos/redux.svg'
+                alt='Redux'
+              />
+              <img
+                aria-labelledby='Sass'
+                src='https://cdn.worldvectorlogo.com/logos/sass-1.svg'
+                alt='Sass'
+              />
+              <img
+                aria-labelledby='MongoDB'
+                src='https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg'
+              />
+              <img
+                aria-labelledby='Amazon Web Services'
+                src='https://cdn.worldvectorlogo.com/logos/aws-2.svg'
+                alt='Amazon Web Services'
+              />
+              <img
+                aria-labelledby='JSONWebToken'
+                src='https://cdn.worldvectorlogo.com/logos/jwtio-json-web-token.svg'
+                alt='JSONWebToken'
+              />
+              <img
+                aria-labelledby='NPM'
+                src='https://cdn.worldvectorlogo.com/logos/npm.svg'
+                alt='NPM'
+              />
+              <img
+                aria-labelledby='Figma'
+                src='https://cdn.worldvectorlogo.com/logos/figma-1.svg'
+                alt='Figma'
+              />
+              <img
+                aria-labelledby='Gatsby'
+                src='https://cdn.worldvectorlogo.com/logos/gatsby.svg'
+                alt='Gatsby'
+              />
+            </div>
           </div>
         </div>
-      </div>
         <div className='fullSection'>
           <div className='fullSectionContent limitWidthContent'>
             <h1 className='bigTitle'>Projects</h1>
@@ -191,50 +245,49 @@ export default class Homepage extends Component {
             <div className='siteInfoHomepage'>
               <div className='siteTitleHomepage'>
                 <h5> Expertise </h5>
-                <h2>I am a full-stack web developer with 2 years of experience
-                    creating web applications using JavaScript and integrated
-                    JavaScript libraries/frameworks.</h2>
+                <h2>
+                  I am a full-stack web developer with 2 years of experience
+                  creating web applications using JavaScript and integrated
+                  JavaScript libraries/frameworks.
+                </h2>
                 <br />
                 <div className='siteTextHomepage'>
                   <p>
                     {' '}
-                    I can design, build and
-                    launch your vision at scale. Whether you're a Fortune 500
-                    company, a startup or even a mom-and-pop shop - I give my
-                    clients the technical prowess they need to succeed.{' '}
+                    I can design, build and launch your vision at scale. Whether
+                    you're a Fortune 500 company, a startup or even a
+                    mom-and-pop shop - I give my clients the technical prowess
+                    they need to succeed.{' '}
                   </p>
                 </div>
               </div>
 
-            <div className='siteButtonsHomepage'>
-              <Link to={'/Projects'}>
-                <button className='circleBtn circleBtnDark'>
-                  <span className='circleBtnIcon'>+</span>
-                  <h5
-                   >
-                    See Case History
-                  </h5>
-                </button>
-              </Link>
-              <Link to={'/Contact'}>
-                <button className='simpleBtn simpleBtnDark'>
-                  <h4 style={{ padding: '8px 100px', borderRadius: '50%' }}>
-                    Contact
-                  </h4>
-                </button>
-              </Link>
+              <div className='siteButtonsHomepage'>
+                <Link to={'/Projects'}>
+                  <button className='circleBtn circleBtnDark'>
+                    <span className='circleBtnIcon'>+</span>
+                    <h5>See Case History</h5>
+                  </button>
+                </Link>
+                <Link to={'/Contact'}>
+                  <button className='simpleBtn simpleBtnDark'>
+                    <h4 style={{ padding: '8px 100px', borderRadius: '50%' }}>
+                      Contact
+                    </h4>
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className='backSvg backRightBottom'>
-            <SvgIcons
-              style={{ textShadow: '0px, 0px, 5px, rgb(240, 248, 255)' }}
-              dimension='600px'
-              iconName='star'
-              fillColor='rgb(240, 248, 255)'
-            />
+            <div className='backSvg backRightBottom'>
+              <SvgIcons
+                style={{ textShadow: '0px, 0px, 5px, rgb(240, 248, 255)' }}
+                dimension='600px'
+                iconName='star'
+                fillColor='rgb(240, 248, 255)'
+              />
+            </div>
           </div>
         </div>
-            </div>
 
         <div className='fullSection'>
           <div className='fullSectionContent limitWidthContent'></div>
@@ -247,7 +300,11 @@ export default class Homepage extends Component {
           <div className='fullSectionContent limitWidthContent thirdBottom'>
             <div className='siteInfo'>
               <div className='siteTitle'>
-                <FontAwesomeIcon icon={faGlobe} className='icon' style={{width: '35px', height: '35px'}} />
+                <FontAwesomeIcon
+                  icon={faGlobe}
+                  className='icon'
+                  style={{ width: '35px', height: '35px' }}
+                />
                 <h5> Full Package </h5>
                 <h2> 360° Digital Presence </h2>
               </div>
